086f8683cd2b7151bc4c23446389665159c6ca3b
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-01-21 22:43:36 +0200

    hw12

--- src/referencesHTML.js
+++
@@ -1,7 +0,0 @@
-export default function reference() {
-  return {
-    countryList: document.querySelector('.list-group'),
-    countryCard: document.querySelector('.card-country'),
-    input: document.querySelector('.form-control'),
-  };
-}
